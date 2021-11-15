802a42e2d7517400f513ff2210651a5f2b1092ab
shivamtripathi19 <shivam.tripathi@tokopedia.com>
2021-11-15 18:59:51 +0530

    feat:form data

--- expense-tracker/src/App.js
+++ expense-tracker/src/App.js
@@ -1,35 +1,41 @@
 import React from "react";
-import Expenses from './components/Expenses';
+import NewExpense from "./components/NewExpense/NewExpense";
+import Expenses from "./components/Expenses";
+const expensesData = [
+  {
+    id: "e1",
+    title: "Toilet Paper",
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+  },
+  { id: "e2", title: "New TV", amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: "e3",
+    title: "Car Insurance",
+    amount: 294.67,
+    date: new Date(2021, 2, 28),
+  },
+  {
+    id: "e4",
+    title: "New Desk (Wooden)",
+    amount: 450,
+    date: new Date(2021, 5, 12),
+  },
+];
 
 function App() {
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-    },
-    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.67,
-      date: new Date(2021, 2, 28),
-    },
-    {
-      id: 'e4',
-      title: 'New Desk (Wooden)',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-    },
-  ];
+  const [expenses, SetExpenses] = React.useState(expensesData);
+
+  const addExpenseHandler = (expense) => {
+    SetExpenses([...expenses, expense]);
+    console.log(expense);
+  };
 
   return (
     <div>
       <h2>Let's get started!</h2>
-      <Expenses
-        expenses={expenses}
-      />
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses expenses={expenses} />
     </div>
   );
 }
